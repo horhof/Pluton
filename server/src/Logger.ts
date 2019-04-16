b348cac7a92c58d022078fade52d8a83d49828cb
Horunabu Hofutenho <horhof@protonmail.com>
2019-04-15 22:15:26 -0400

    Remove prior server implementation

--- server/src/Logger.ts
+++
@@ -1,3 +0,0 @@
-import * as Debug from 'debug'
-
-export const getLog = (tag: string) => Debug(`Pluton:${tag}`)
--- server/src/Types.ts
+++
@@ -1,5 +0,0 @@
-import * as the from 'lodash'
-
-export type Id = string
-
-export const isId = (x: any): boolean => the(x).isString()
