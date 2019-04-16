ef76f2e487be9b2a9be6e10731d5da1ebd223c0c
Horunabu Hofutenho <horhof@protonmail.com>
2019-04-15 22:14:57 -0400

    Remove client

--- client/src/app/store/Fleet.ts
+++
@@ -1,9 +0,0 @@
-interface FleetStore extends Ext.data.IStore {
-}
-
-(Ext.define as (a: string, b: FleetStore) => void)(`Pluton.store.Fleet`, {
-  extend: `Ext.data.Store`,
-  alias: [`store.fleet`],
-  model: `Pluton.model.Fleet`,
-  autoLoad: true,
-})
--- client/src/app/store/User.ts
+++
@@ -1,9 +0,0 @@
-interface UserStore extends Ext.data.IStore {
-}
-
-(Ext.define as (a: string, b: UserStore) => void)(`Pluton.store.User`, {
-  extend: `Ext.data.Store`,
-  alias: [`store.user`],
-  model: `Pluton.model.User`,
-  autoLoad: true,
-})
