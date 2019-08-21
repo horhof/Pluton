abd891af8dd7136e490f965ccc3a5f37a595532d
Horunabu Hofutenho <horhof@protonmail.com>
2019-08-20 22:12:02 -0400

    Add edit fleet

--- src/Routes.ts
+++ src/Routes.ts
@@ -1,17 +1,17 @@
-import Router from 'koa-router'
 import koaBody from 'koa-body'
+import Router from 'koa-router'
+import * as FleetCtrl from './controllers/FleetCtrl'
 import * as PlanetCtrl from './controllers/PlanetCtrl'
 import * as StarCtrl from './controllers/StarCtrl'
 
 const bind =
   (router: Router, prefix: string) => {
+    router.get(`${prefix}/clusters/:index.html`, StarCtrl.readCluster)
+    router.get(`${prefix}/stars/:id.html`, StarCtrl.readStar)
     router.get(`${prefix}/planets/new.html`, PlanetCtrl.createPlanetForm)
     router.post(`${prefix}/planets/new.json`, koaBody(), PlanetCtrl.createPlanet)
     router.get(`${prefix}/planets/:id.html`, PlanetCtrl.readPlanet)
-
-    router.get(`${prefix}/stars/:id.html`, StarCtrl.readStar)
-
-    router.get(`${prefix}/clusters/:index.html`, StarCtrl.readCluster)
+    router.get(`${prefix}/fleets/:id.html`, FleetCtrl.readFleet)
   }
 
 export default bind
--- src/controllers/FleetCtrl.ts
+++ src/controllers/FleetCtrl.ts
@@ -1,9 +1,39 @@
-import { get } from 'lodash'
+import { chain, get } from 'lodash'
 import { query } from '../Database'
 import { stampLog } from '../Log'
-import { Ctx } from '../Server'
+import { Ctx, showErr } from '../Server'
+import { Fleet } from '../Fleet';
 
 const log = stampLog(`Http:Fleet`)
+
+/** GET /fleets/1.html */
+export const readFleet =
+  async (ctx: Ctx): Promise<void> => {
+    const $ = log(`readFleet`)
+
+    const id = chain(ctx.params).get('id').toNumber().value()
+
+    if (!isFinite(id)) {
+      return showErr(ctx, `"${id}" is not a valid fleet ID.`, $, 400)
+    }
+
+    const res = await query({ noun: `fleets?id=eq.${id}` })
+
+    if (!res.ok) {
+      return showErr(ctx, `Can't find fleet "${id}" not valid.`, $, 404)
+    }
+
+    const data = await res.json() as Fleet[]
+
+    if (data.length < 1) {
+      return showErr(ctx, `Can't find fleet "${id}".`, $, 404)
+    }
+
+    const input = data[0]
+    const template = require('../templates/Fleet.marko')
+    ctx.type = 'html'
+    ctx.body = template.stream(input)
+  }
 
 /** GET /fleets/new.html */
 export const createFleetForm =
